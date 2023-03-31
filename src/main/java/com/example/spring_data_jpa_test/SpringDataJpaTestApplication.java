7738a8a49dedaa3d3630610311bc6ab855369daa
jsyoon <jsyoon@neighbor21.co.kr>
2023-03-31 15:31:04 +0900

    :tada:

---
+++ src/main/java/com/example/spring_data_jpa_test/SpringDataJpaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_data_jpa_test/SpringDataJpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
